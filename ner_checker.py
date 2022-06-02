9c36c94705a5b2241feecafc8a6415a228ca6b9c
fojcipaw <106378110+fojcipaw@users.noreply.github.com>
2022-06-02 14:05:40 +0200

    corrections

--- ner_checker.py
+++ ner_checker.py
@@ -1,4 +1,3 @@
-!pip install conllu
 from conllu import parse
 
 class NerChecker:
@@ -39,3 +38,72 @@ class NerChecker:
 
     #comparate members
     self.compare_list = []
+
+  def get_text(self):
+    return self.text
+
+  def prepare_comparison(self, doc, label_mapping_dict):
+    for ent in doc.ents:
+      entity = ent.text
+      label = ent.label_
+      self.__add_entity(entity.replace(" ","_"), label, label_mapping_dict)
+
+  def get_compare_list(self):
+    return self.compare_list
+  
+  def show_statistics(self):
+    positive=0
+    negative=0
+    not_found=0
+    for compare_list in self.get_compare_list():
+      if compare_list["found"]:
+        if compare_list["match"]:
+          positive+=1
+        else:
+          negative+=1
+      else:
+        not_found+=1
+    print({"positive":positive,"negative":negative,"not_found":not_found})
+  
+  def show_in_array(self):
+    import pandas as pd
+
+    oryg_entities = []
+    oryg_labels = []
+    lib_entities = []
+    lib_labels = []
+    founds = []
+    matches = []
+
+    for compare_list in self.get_compare_list():
+      oryg_entities.append(compare_list["oryg_entity"])
+      oryg_labels.append(compare_list["oryg_label"])
+      lib_entities .append(compare_list["entity"])
+      lib_labels.append(compare_list["label"])
+      founds.append(compare_list["found"])
+      matches.append(compare_list["match"])
+
+    return pd.DataFrame({'oryg_entity':oryg_entities,'oryg_label':oryg_labels,'entity':lib_entities, 'label':lib_labels,'found':founds,'match':matches})
+
+  def __add_entity_to_data(self, oryg_entity, oryg_label, entity, label, label_mapping_dict):
+    match = False
+    if oryg_label in label:
+      match = True
+    else:
+      if oryg_label in label_mapping_dict:
+        mapped_label = label_mapping_dict[oryg_label]
+        if mapped_label in label:
+          match = True
+    if match:
+      self.compare_list.append({"entity":entity,"label":label, "oryg_entity":oryg_entity, "oryg_label":oryg_label,"found":True,"match":True})
+    else:
+      self.compare_list.append({"entity":entity,"label":label, "oryg_entity":oryg_entity, "oryg_label":oryg_label,"found":True,"match":False})
+
+  def __add_entity(self, entity, label, label_mapping_dict):
+    for oryg_entity, oryg_label in self.data.items():
+      if oryg_entity in entity:
+        self.__add_entity_to_data(oryg_entity, oryg_label, entity, label, label_mapping_dict)
+        return
+      else:
+        pass
+    self.compare_list.append({"entity":entity,"label":label, "oryg_entity":None, "oryg_label":None,"found":False,"match":False})
