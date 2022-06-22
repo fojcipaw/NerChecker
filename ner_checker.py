cd7c951a3c0032868c26ace1136b2dd05d494a41
fojcipaw <106378110+fojcipaw@users.noreply.github.com>
2022-06-22 16:30:29 +0200

    new version, added measurements

--- ner_checker.py
+++ ner_checker.py
@@ -1,128 +1,184 @@
+!pip install conllu
 from conllu import parse
+import pandas as pd
+import time
 
 class NerChecker:
-  def __init__(self, path_to_conllu_file):
+  def __init__(self, file_type="conllu", path_to_file):
+    if file_type=="conllu":
+        sentences = self.__conllu_prepare_sentences(path_to_file)
+        self.__conllu_prepare_text(sentences)
+    else:
+        pass
+    self.data = {}
+  
+  def nlp(self, nlp):
+    st = time.time()
+    doc = nlp(self.get_text())
+    et = time.time()
+    elapsed_time = et - st
+    return doc, elapsed_time
+  
+  def get_df(self, lib_name):
+    oryg_entities = []
+    oryg_labels = []
+    lib_entities = []
+    lib_labels = []
+    estimates = []
+    for d in self.data[lib_name]["data"]:
+        oryg_entities.append(d[0])
+        oryg_labels.append(d[1])
+        lib_entities.append(d[2])
+        lib_labels.append(d[3])
+        estimates.append(d[4])
+        
+        df = pd.DataFrame({'oryg entity':oryg_entities,'oryg label':oryg_labels,lib_name+' entity':lib_entities, lib_name+' label':lib_labels, 'estimates':estimates})
+    return df
+
+  def get_text(self):
+    return self.text
+    
+  def get_measurements(self):
+    import math
+    
+    data={}
+    for lib_name, value in self.data.items():
+        qmc = value["qmc"]
+        lib_elapsed_time = value["elapsed_time"]
+        #print(qmc)
+        #	(sensitivity, recall) true positive rate = TP/(TP+FN) = 1 − false negative rate
+        recall = true_positive_rate=qmc['True Positive']/(qmc['True Positive']+qmc['False Negative'])
+        #print("sensitivity = recall = true_positive_rate",true_positive_rate)
+        #	(specificity) false positive rate = FP/(FP+TN) 
+        false_positive_rate=qmc['False Positive']/(qmc['False Positive']+qmc['True Negative'])
+        #print("specificity = false_positive_rate",false_positive_rate)
+        # (precision) positive predictive value = TP/(TP+FP)
+        precision = positive_predictive_value = qmc['True Positive']/(qmc['True Positive']+qmc['False Positive'])
+        #print("precision = positive_predictive_value",positive_predictive_value)
+        #F-score is the harmonic mean of precision and recall
+        f_score = 2*math.sqrt((precision*recall)/(precision+recall))
+        #print("F-score",f_score)
+        #G-score is the geometric mean of precision and recall
+        g_score = math.sqrt(precision*recall)
+        #print("G-score",g_score)
+        #Accuracy
+        accuracy=(qmc['True Positive']+qmc['True Negative'])/(qmc['True Positive']+qmc['False Positive']+qmc['True Negative']+qmc['False Negative'])
+        data.update({lib_name:[true_positive_rate,false_positive_rate,positive_predictive_value,f_score,g_score,accuracy, lib_elapsed_time]})
+
+    df = pd.DataFrame(data)
+    df.index=["true_positive_rate (recall, sensitivity)","false_positive_rate (specificity)", "positive_predictive_value (precision)","f_score","g_score","accuracy","elapsed time"]
+    return df
+
+  def compare(self, lib_name, elapsed_time, doc, label_mapping):
     """
-    1) read annotations from path_to_conllu_file
-    2) prepare data and text
-    3) prepare obj variables
+    function to prepare comparison
+    lib_name: name of the nlp library, eg "spacy", "stanza"
+    doc : nlp object
+    label_mapping : dict (mapping between oryginal label and nlp label, eg {"PER":"PERSON"})
     """
-    #1) read annotations
+    data = []
+    qmc={"True Positive":0,"True Negative":0,"False Positive":0,"False Negative":0} #quality measurement counter
+    base_idx = 0
+    for o in self.oryg:
+        o_entity = o[0]
+        o_label = o[1]
+        is_added = False
+
+        for idx in range(base_idx,len(doc.ents)):
+            l_entity = doc.ents[idx].text
+            l_label = self.__get_label(doc.ents[idx])
+            if self.__is_entity_eq(o_entity, l_entity):
+
+                for j in range(base_idx+1,idx):
+                    lj_entity = doc.ents[j].text
+                    lj_label = self.__get_label(doc.ents[j])
+                    data.append((None,None,lj_entity,lj_label,"True Negative"))
+                    qmc["True Negative"]+=1
+
+                if self.__is_label_eq(o_label, l_label,label_mapping):
+                    data.append((o_entity,o_label,l_entity,l_label,"True Positive"))
+                    qmc["True Positive"]+=1
+
+                else:
+                    data.append((o_entity,o_label,l_entity,l_label,"False Positive"))
+                    qmc["False Positive"]+=1
+
+                base_idx = idx+1
+                is_added = True
+                break
+
+            else:
+                pass
+
+        if is_added == False:
+            data.append((o_entity,o_label,None,None,"False Negative"))
+            qmc["False Negative"]+=1
+    self.data.update({lib_name:{"data":data, "qmc":qmc, "elapsed_time": elapsed_time}})
+
+  def __conllu_prepare_sentences(self, path_to_file):
     annotations = None
-    with open(path_to_conllu_file) as f:
+    with open(path_to_file) as f:
         annotations = f.read()
     #parse annotations, get sentences
     sentences = parse(annotations)
-
-    #2) prepare:
-    # - self.data
-    # - self.text
-    self.data={}
+    return sentences
+  
+  def __conllu_prepare_text(self, sentences):
+    self.oryg=[]
     form_list=[]
-    form_list_all=[]
-    for sentence in sentences:
+    text_list=[]
+    #for sentence in sentences[:100]: #for test purpose
+    for sentence in sentences[:10]:
       for token in sentence:
         id = token['id']
         lemma = token['lemma']
         form = token['form']
-        form_list_all.append(form)
+        text_list.append(form)
 
         if lemma[0:2] == "B-":
           if form_list:
-            self.data.update({"_".join(form_list):lemma_saved})
+            self.oryg.append(("_".join(form_list),lemma_saved))
           form_list = [form]
           lemma_saved = lemma[2:]
         elif lemma[0:2] == "I-":
           form_list.append(form)
         else:
           if form_list:
-            self.data.update({"_".join(form_list):lemma_saved})
+            self.oryg.append((" ".join(form_list),lemma_saved))
           form_list = []
           lemma_saved = ""
-    self.text = " ".join(form_list_all)
+    self.text = " ".join(text_list)
 
-    #3) compare members
-    self.compare_list = []
-    self.statistics = []
-
-  def get_text(self):
-    """
-    getter (text)
-    """
-    return self.text
-
-  def prepare_comparison(self, doc, label_mapping_dict):
-    """
-    function to prepare comparison
+  def __is_entity_eq(self, value_first, value_second):
+    if value_first == value_second:
+        return True
+    elif value_first == "the "+value_second:
+        return True
+    elif value_first == "The "+value_second:
+        return True
+    elif "the "+value_first == value_second:
+        return True
+    elif "The "+value_first == value_second:
+        return True
+    else:
+        return False
+  
+  def __is_label_eq(self, o_label, l_label, label_mapping):
+    if o_label == l_label:
+        return True
     
-    doc : nlp object
-    label_mapping_dict : dict (mapping between oryginal label and nlp label which should be equal)
-    """
-    for ent in doc.ents:
-      entity = ent.text
-      label = ent.label_
-      self.__add_entity(entity.replace(" ","_"), label, label_mapping_dict)
-
-  def get_results_statistics(self):
-    """
-    get result information about how many positive,negative, not matched
-    """
-    positive=0
-    negative=0
-    not_found=0
-    for compare_record in self.compare_list:
-      if compare_record["found"]:
-        if compare_record["match"]:
-          positive+=1
-        else:
-          negative+=1
-      else:
-        not_found+=1
-    return {"positive":positive,"negative":negative,"not_found":not_found}
-
-  def get_results_data_frame(self):
-    """
-    get result information in data frame
-    """
-    import pandas as pd
-
-    oryg_entities = []
-    oryg_labels = []
-    lib_entities = []
-    lib_labels = []
-    founds = []
-    matches = []
-
-    for compare_list in self.compare_list:
-      oryg_entities.append(compare_list["oryg_entity"])
-      oryg_labels.append(compare_list["oryg_label"])
-      lib_entities .append(compare_list["entity"])
-      lib_labels.append(compare_list["label"])
-      founds.append(compare_list["found"])
-      matches.append(compare_list["match"])
-
-    return pd.DataFrame({'oryg_entity':oryg_entities,'oryg_label':oryg_labels,'entity':lib_entities, 'label':lib_labels,'found':founds,'match':matches})
-
-  def __add_entity_to_data(self, oryg_entity, oryg_label, entity, label, label_mapping_dict):
-    match = False
-    if oryg_label in label:
-      match = True
-    else:
-      if oryg_label in label_mapping_dict:
-        mapped_label = label_mapping_dict[oryg_label]
-        if mapped_label in label:
-          match = True
-    if match:
-      self.compare_list.append({"entity":entity,"label":label, "oryg_entity":oryg_entity, "oryg_label":oryg_label,"found":True,"match":True})
-    else:
-      self.compare_list.append({"entity":entity,"label":label, "oryg_entity":oryg_entity, "oryg_label":oryg_label,"found":True,"match":False})
-
-
-  def __add_entity(self, entity, label, label_mapping_dict):
-    for oryg_entity, oryg_label in self.data.items():
-      if oryg_entity in entity:
-        self.__add_entity_to_data(oryg_entity, oryg_label, entity, label, label_mapping_dict)
-        return
-      else:
-        pass
-    self.compare_list.append({"entity":entity,"label":label, "oryg_entity":None, "oryg_label":None,"found":False,"match":False})
+    if o_label in label_mapping and label_mapping[o_label] == l_label:
+      return True
+    
+    if l_label in label_mapping and label_mapping[l_label] == o_label:
+      return True
+    
+    return False
+  
+  def __get_label(self, entity):
+    label = ""
+    try:
+      label = entity.label_
+    except:
+      label = entity.type
+    return label
